0f7c5c747b52f31e97aae343fb6645dc2bc63efa
lzl <445199115@qq.com>
2025-08-15 15:25:56 +0800

    一次性导入所有通用scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
-import "./style/global.scss";
+import.meta.glob("./style/*.scss", { eager: true });
 
 import App from "./App.vue";
 const app = createApp(App);
@@ -22,9 +22,9 @@ app.use(ElementPlus, {
 import Echart from "@/components/Echart.vue";
 app.component("Echart", Echart);
 
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
+  app.component(key, component);
 }
 
 app.mount("#app");
